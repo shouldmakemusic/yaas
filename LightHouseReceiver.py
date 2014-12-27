78892380fd8802dfa0456257a889eccc315554aa
Manuel Hirschauer <manuel@hirschauer.net>
2014-12-27 17:03:42 +0100

    React to Midi from Lighthouse

--- LightHouseReceiver.py
+++ LightHouseReceiver.py
@@ -7,6 +7,7 @@ import Live
 """ Constants and configuration """
 from consts import *
 from config_lighthouse import *
+from util.RangeUtil import RangeUtil
 
 class LightHouseReceiver:
     __module__ = __name__
@@ -15,10 +16,16 @@ class LightHouseReceiver:
     def __init__(self, parent, c_instance):
         self._LightHouseReceiver__c_instance = c_instance
         self._parent = parent
+        self._range_util_android = RangeUtil(0, 100)
 
     def build_midi_map(self, midi_map_handle):
 
         print('(LightHouseReceiver) build_midi_map() called')
+
+        Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 1)
+        Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 2)
+        Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 3)
+        Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 4)
         
         # midi_note_definitions
         for k, v in midi_note_definitions_lighthouse.iteritems():
@@ -38,17 +45,53 @@ class LightHouseReceiver:
             message_type = midi_bytes[0]
             midi_note = midi_bytes[1]
             value = midi_bytes[2]
-            print('found message_type ' + str(message_type)); 
+            #print('found message_type ' + str(message_type)); 
             if (message_type == MESSAGE_TYPE_LIGHTHOUSE_MIDI_NOTE_PRESSED):
-                print('found pressed'); 
+                #print('found pressed'); 
                 if (midi_note in midi_note_definitions_lighthouse):
                     print('found action');                    
                     self._parent.handle_parametered_function(midi_note_definitions_lighthouse, midi_note, value);
-                
+                if (midi_note == 1):
+                    if (value == 1):                        
+                        #print('found start')
+                        track_helper = self._parent._song_helper.get_track_for_name("Alto Flute")
+                        if (track_helper is not None):
+                            #print('track: ' + track_helper.get_track().name)
+                            #device = track_helper.get_device("Flute")
+                            track_helper.get_track().clip_slots[1].fire()
+                    if (value == 2):                        
+                        #print('found stop')
+                        track_helper = self._parent._song_helper.get_track_for_name("Alto Flute")
+                        if (track_helper is not None):
+                            #print('track: ' + track_helper.get_track().name)
+                            #device = track_helper.get_device("Flute")
+                            track_helper.get_track().clip_slots[1].stop()
+                if (midi_note == 2):
+                    #print('x: ' + str(value))
+                    self.handleKnob(1, value)
+                if (midi_note == 3):
+                    #print('y: ' + str(value))
+                    self.handleKnob(2, value)
+                if (midi_note == 4):
+                    #print('z: ' + str(value))
+                    self.handleKnob(3, value)
+
             elif (message_type == MESSAGE_TYPE_LIGHTHOUSE_MIDI_NOTE_RELEASED):
-                #self.log_message("Button released");
+                #self.log_message("released");
                 return
         return
+    
+    def handleKnob(self, type, value):
+        
+        track_helper = self._parent._song_helper.get_track_for_name("Alto Flute")
+        if (track_helper is not None):
+            device = track_helper.get_device("Flute")
+            if (device is not None):
+                #print('found hash device: ' + device.name)         
+                
+                valueX = self._range_util_android.get_value(device.parameters[type], value);
+                #print('valueX ' + str(valueX))
+                device.parameters[type].value = valueX
 
     def script_handle(self):
         return self._LightHouseReceiver__c_instance.handle()
