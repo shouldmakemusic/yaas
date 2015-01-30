186d0743460d05f372b2f63e18747a8ef226d5b9
Manuel Hirschauer <manuel@hirschauer.net>
2015-01-30 23:09:16 +0100

    midi commands for starting tracks

--- LightHouseReceiver.py
+++ LightHouseReceiver.py
@@ -17,6 +17,7 @@ class LightHouseReceiver:
         self._LightHouseReceiver__c_instance = c_instance
         self._parent = parent
         self._range_util_android = RangeUtil(0, 100)
+        self._currentClipNumber = 0
 
     def build_midi_map(self, midi_map_handle):
 
@@ -26,6 +27,9 @@ class LightHouseReceiver:
         Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 2)
         Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 3)
         Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 4)
+        Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 12)
+        Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 13)
+        Live.MidiMap.forward_midi_note(self.script_handle(), midi_map_handle, CHANNEL_LIGHTHOUSE, 14)
         
         # midi_note_definitions
         for k, v in midi_note_definitions_lighthouse.iteritems():
@@ -75,12 +79,39 @@ class LightHouseReceiver:
                 if (midi_note == 4):
                     #print('z: ' + str(value))
                     self.handleKnob(3, value)
+                if (midi_note == 12):
+                   print('x: ' + str(value))
+                   self.add_second_track(value)
+                if (midi_note == 13):
+                    #print('y: ' + str(value))
+                    self.startClip(value)
+                #if (midi_note == 14):
+                #    print('y: ' + str(value))
 
             elif (message_type == MESSAGE_TYPE_LIGHTHOUSE_MIDI_NOTE_RELEASED):
                 #self.log_message("released");
                 return
         return
-    
+    def add_second_track(self, value):
+        
+        track_helper = self._parent._song_helper.get_track_for_name("DrumBeat")
+        if (track_helper is not None):
+            value = value - 70
+            if (value > 5):
+                track_helper.get_track().clip_slots[self._currentClipNumber].fire()
+            if (value < 5):
+                track_helper.get_track().clip_slots[self._currentClipNumber].stop()
+                
+    def startClip(self, value):
+        track_helper = self._parent._song_helper.get_track_for_name("DrumNorm")
+        if (track_helper is not None):
+            value = value - 70
+            if (value != self._currentClipNumber):
+                self._currentClipNumber = value
+                track_helper.get_track().clip_slots[value].fire()
+            #print('track: ' + track_helper.get_track().name)
+            
+            
     def handleKnob(self, type, value):
         
         track_helper = self._parent._song_helper.get_track_for_name("Alto Flute")
