4f479c62410dc1cdff538995241071e5b591f263
Manuel Hirschauer <manuel@hirschauer.net>
2015-03-27 15:36:11 +0100

    added new methods (still to test)

--- controller/TrackController.py
+++ controller/TrackController.py
@@ -37,7 +37,7 @@ class TrackController (YaasController):
 		"""
 		self.log.verbose("(TrackController) stop_or_restart_clip called")
 		track_index = params[0]
-		self.log.verbose("(TrackController) for clip " + str(track_index))
+		self.log.verbose("(TrackController) for track " + str(track_index))
 
 		track_helper = self.track_helper(track_index)
 		track_helper.stop_or_restart_clip()
@@ -50,7 +50,7 @@ class TrackController (YaasController):
 
 		self.log.verbose("(TrackController) stop called")
 		track_index = params[0]
-		self.log.verbose("(TrackController) for clip " + str(track_index))
+		self.log.verbose("(TrackController) for track " + str(track_index))
 
 		track_helper = self.track_helper(track_index)
 		track_helper.get_track().stop_all_clips()
@@ -63,7 +63,7 @@ class TrackController (YaasController):
 
 		self.log.verbose("(TrackController) arm called")
 		track_index = params[0]
-		self.log.verbose("(TrackController) for clip " + str(track_index))
+		self.log.verbose("(TrackController) for track " + str(track_index))
 
 		track_helper = self.track_helper(track_index)
 		track_helper.arm()
@@ -76,7 +76,84 @@ class TrackController (YaasController):
 
 		self.log.verbose("(TrackController) get_focus called")
 		track_index = params[0]
-		self.log.verbose("(TrackController) for clip " + str(track_index))
+		self.log.verbose("(TrackController) for track " + str(track_index))
 		
 		track = self.track_helper(track_index).get_track()
 		self.view_helper().focus_on_track(track)
+
+	def toggle_mute_track(self, params, value):
+		"""
+			Toggles the muted state for the given track
+			0 -> track_index
+		"""
+
+		self.log.verbose("(TrackController) toggleMuteTrack called")
+		track_index = params[0]
+		self.log.verbose("(TrackController) for track " + str(track_index))
+		
+		track = self.track_helper(track_index).get_track()
+		muted = track.mute
+		if muted:
+			track.mute = 0
+		else:
+			track.mute = 1
+
+	def toggle_solo_track(self, params, value):
+		"""
+			Toggles the soloed state for the given track
+			0 -> track_index
+		"""
+		self.log.verbose("(TrackController) toggle_solo_track called")
+		track_index = params[0]
+		self.log.verbose("(TrackController) for track " + str(track_index))
+
+		track = self.track_helper(track_index).get_track()
+		if track.solo:
+			track.solo = 0
+		else:
+			track.solo = 1
+
+	def set_pan(self, params, value):
+		"""
+			Sets the pan for the given track	
+			0 -> track_index
+		"""
+		self.log.verbose("(TrackController) set_pan called")
+		track_index = params[0]
+		self.log.verbose("(TrackController) for track " + str(track_index))
+
+		self.track_helper(track_index).get_track().value = value
+		
+	def set_send(self, params, value):
+		"""
+			Sets the send value for the given track
+			0 -> track_index
+			1 -> send_index
+		"""
+		self.log.verbose("(TrackController) set_send called")
+		track_index = params[0]
+		send_index = params[1]
+		self.log.verbose("(TrackController) for track " + str(track_index) + " and send " + str(send_index))
+		
+		track_helper = self.song_helper().get_track(track_index)		
+		new_value = track_helper.get_normalized_value_from_target(track_helper.get_track().mixer_device.sends[send_id], value)		
+		#self.log.verbose("set send " + str(send_index) + " for track " + str(track_index) + " to value " + str(new_value))
+		track_helper.set_send_value(send_index, new_value)
+		
+	def set_volume(self, params, value):
+		"""
+			Sets the volume for the given track
+			0 -> track_index
+		"""
+		self.log.verbose("(TrackController) set_volume called")
+		track_index = params[0]
+		self.log.verbose("(TrackController) for track " + str(track_index))
+
+		track_helper = self.song_helper().get_track(track_index)
+		#self.log.debug("Volume note code " + str(value) + " and value " + str(midi_bytes[2]))
+		# value is between 0 and 127 - for volume the wanted max value is 0.85
+		value = (0.85 * value) / 128.0
+		selected_track = track_helper.get_track()
+		selected_track.mixer_device.volume.value = value
+
+
